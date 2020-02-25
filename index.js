39cd8c1592ea4572502d1d7df91bbcc09d109079
Mokoolaid <55265990+Mokoolaid@users.noreply.github.com>
2020-02-24 18:36:46 -0800

    tried chaning again
    pushing to branch this time

--- index.js
+++ index.js
@@ -17,34 +17,54 @@ function populateBtn() {
 
 //function to play or a srtill frame GIF Images
 
-function displayGiph() {
-    var name = $(this).attr("data-name")
-
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=HairStyles&apikey7q1zHp1SJhZjsopeU74JTrhL3IPNlws8limit=20");
-    xhr.done(function (data) { console.log("success got data", data); });
-
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", still)
-    }
-
-}
-
 $("#add-hairstyle").on("click", function (event) {
     event.preventDefault()
     var newHairStyle = $("#hairstyles-input").val().trim()
     hairstyles.push(newHairStyle)
     populateBtn()
+
     var type = $(this).attr("data-type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api-key=ha7q1zHp1SJhZjsopeU74JTrhL3IPNlws8&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
 
-})
+            var results = response.data;
+
+            hairstyles.forEach(element => {
+                var button = $("<button>")
+                /* button.attr("class", "hairstyle-btn")
+                 button.text(element)
+                 $("#hairstyles-btn").append(button)*/
+                var rating = hairstyles["afro"].rating;
+                var p = $("<p>").text("Rating: " + rating);
 
 
+                var animated = results["afro"].images.fixed_height.url;
+                var still = results["afro"].images.fixed_height_still.url
 
-hairstyles.forEach(element => {
-    var button = $("<button>")
-    button.attr("class", "hairstyle-btn")
-    button.text(element)
-    $("#hairstyles-btn").append(button)
+                var hairStyleImage = $("<img>");
+                hairStyleImage.attr("src, still")
+                hairStyleImage.attr("data-still", still);
+                hairStyleImage.attr("data-animate", animated);
+                hairStyleImage.attr("data-state", "still");
+                hairStyleImage.addClass("hairStyle-image");
+
+                hairStyleDiv.append(p);
+                hairStyleDiv.append(hairStyle - image)
+                $("#hairstyles").append("hairstyle-btn")
+            });
+
+            hairstyles.forEach(element => {
+                var button = $("<button>")
+                button.attr("class", "hairstyle-btn")
+                button.text(element)
+                $("hairstyles-btn").append(button)
+            }
+
+            );
+        });
+    populateBtn()
 });
-populateBtn()
